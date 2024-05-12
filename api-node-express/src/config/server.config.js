5feafcaaca924832625ad749cfef14c44887a6d9
Cristina <cristinabosasanchez@gmail.com>
2024-05-12 13:40:30 +0100

    se ha añadido cors para poder conectar la api a un front

--- api-node-express/src/config/server.config.js
+++ api-node-express/src/config/server.config.js
@@ -1,13 +1,14 @@
 import express from "express";
 import animeRouter from "../routers/anime.route.js";
 import bodyParser from "body-parser";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use("/api/anime", animeRouter);
 
 export default app;
